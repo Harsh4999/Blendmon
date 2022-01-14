f906aeaf01a9cd5c273d3b4d934820420709bd3f
crysosancher <vibhupandey.vp@gmail.com>
2022-01-14 15:46:32 +0530

    Changed the arch

--- Backend/server.js
+++ Backend/server.js
@@ -5,17 +5,20 @@ const youtube = require('./youtube');
 const path=require('path')
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(express.static(path.join(__dirname, '../Public')));
 console.log(__dirname+'/../Public');
 const PORT=3000;
 //app.use(bodyparser.urlencoded());
 app.get('/',(req,res,next)=>{
-  res.sendFile(path.resolve('Backend/index.html'));
+  res.render('index');
   // res.render('index')  
  // next();
 });
 app.get('/you',(req,res,next)=>{
-   res.sendFile(path.resolve('Backend/youtube.html'));
+   res.render('youtube');
 })
 app.get('/youtube',async (req,res,next)=>{
   // const songName = req.params.name;
@@ -25,7 +28,7 @@ app.get('/youtube',async (req,res,next)=>{
    await youtube.video(req.query.fname).then((filePath)=>{
       console.log(filePath);
       console.log('Downloaded');
-      res.sendFile(path.resolve('Backend/index.html'));
+      res.render('getDownload');
       //res.sendFile(`/home/harsh/Blendmon/Blendmon/Backend/93971.mp4`);
    })
  
