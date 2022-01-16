0f87b7acfd1d99ffa85a2fa4f3a6e39a3588e62b
Harsh4999 <trivediharsh49@gmail.com>
2022-01-16 21:23:27 +0530

    insta post resolved

--- Backend/news.js
+++ Backend/news.js
@@ -7,16 +7,7 @@ module.exports.getNews = async () => {
     const { data } = await axios.get(url);
   // console.log(data);
 
-<<<<<<< HEAD
-=======
-    let news = "☆☆☆☆☆💥 Tech News 💥☆☆☆☆☆ \n\n";
-//     data["inshorts"].forEach((headline) => {
-//         count += 1
-//         if (count > 13) return;
-//         news = news + "🌐 " + headline + "\n\n";
-//     });
-    // return data["inshorts"];
->>>>>>> 62006cdbddbc2c65693ffcc4081ba72f6c10c2d3
+
     const obj = {
         "inshorts": data["inshorts"],
         "techradar": data["gadgets-now"],
